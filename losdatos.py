5efb2e864586dcfab44e38dded99044cb6773b48
FRANCY <francypineda@gmail.com>
2016-09-18 23:07:48 -0500

    algunos cambios sobre olos nombres

--- losdatos.py
+++ losdatos.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
-
-
-
-file = open("C:\\Users\\ASUS\\Desktop\\Downloads\\Taller1BigData-master\\Taller1BigData-master\\static\\js\\salida.txt", "r")
+import json
+print("un texto ")
+n=1
+p=3
+print (n+p)
+file = open("C:\\Users\\ASUS\\Desktop\\Downloads\\Taller1BigData-master\\Taller1BigData-master\\static\\js\\salida.txt","r")
+d={}
 for line in file:
-	x = line.split(",")
-	print (x[0],'\t',x[1])
+    x = line.split(",")
+    print(x[0],"nacio",x[2])
+
+
+
+    # print (x[0],'\n',x[1])
+    #print ({x[0],x[1]})
+
+    #print (x[0],'\t',x[1])
+
+
